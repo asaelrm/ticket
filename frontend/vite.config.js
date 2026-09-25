608b7a904ece20df9135683ae1476f7899b38739
Luis Rosario <asaelrm0233@gmail.com>
2026-09-24 21:11:48 -0400

    Auto backup 2026-09-24 21:11:48

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,4 +17,9 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
 });
