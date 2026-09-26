05ed829cb0561b18f034e3279691b6f7c2c2ec99
Luis Rosario <asaelrm0233@gmail.com>
2026-09-25 23:01:45 -0400

    Auto backup 2026-09-25 23:01:45

--- frontend/src/pages/Inbox.jsx
+++ frontend/src/pages/Inbox.jsx
@@ -1,4 +1,4 @@
-﻿import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useTicketEventInvalidator } from '../lib/ticketEvents';
